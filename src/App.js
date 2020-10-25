75726c474c5618ad49a97e8a1c5a1b963226a504
Shashi <sasiwebdev7@gmail.com>
2020-10-25 20:39:25 +0000

    Setting up Firebase & Authentication

--- src/App.js
+++ src/App.js
@@ -6,17 +6,44 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
 import Product from "./Product";
+import Login from "./Login";
 
 function App() {
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/">
             <Header />
             <Home />
             <Product />
-            </Route>
+          </Route>
         </Switch>
       </Router>
     </div>
